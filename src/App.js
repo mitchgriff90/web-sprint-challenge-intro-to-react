64d797d7a8a83dbb2a58745edbedb7fe3dca0c23
Mitchell <mitchell.griffith1990@gmail.com>
2021-05-30 12:19:32 -0700

    mitchell-griffith

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [info, setInfo] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people')
+      .then((res) => {
+        setInfo(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, [])
+  console.log(info);
+
+  const MyStyle = styled.div`'
+    display: flex; 
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    height: 400px;
+    `;
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars!</h1> 
+      <MyStyle>
+        {info.map((ch) => {
+          return <Character key={ch.id} character={ch} />;
+        })}
+      </MyStyle>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const Style = styled.div`
+color: white;
+text-shadow: 2px 2px 5px red;
+`;
+
+const Character = (props) => {
+return(
+    <Style>
+       <p><b>Name:</b> {props.character.name}</p> 
+          <p><b>Birth Year:</b> {props.character.birth_year}</p>
+          <p><b>Gender:</b> {props.character.gender}</p>
+          <p><b>Height:</b> {props.character.height}</p> 
+    </Style>
+)
+};
+
+export default Character;
